1b8cef9101fea7c65e835ab5c91d4058a3bd3768
adminpremsan <146358301+adminpremsan@users.noreply.github.com>
2024-08-19 00:05:42 +0800

    fix gradle files

---
+++ base/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("com.diffplug.spotless")
+}
+
+group = "com.premsan.datastore.base"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+spotless {
+    java {
+        googleJavaFormat("1.19.2").aosp().reflowLongStrings().skipJavadocFormatting()
+        removeUnusedImports()
+        formatAnnotations()
+
+        licenseHeaderFile("license-header")
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("com.diffplug.spotless") version "6.25.0"
 }
 
+group = "com.premsan.datastore"
+version = "0.0.1-SNAPSHOT"
+
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
